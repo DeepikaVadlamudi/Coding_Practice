ff905cf8de92621fd992f3e06036f089f8b5c55c
Deepika (Sesha) Vadlamudi <deepika.vadlamudi77@gmail.com>
2023-10-04 10:57:46 -0700

    Create 941_Valid_Mountain_Array.py

---
+++ Arrays/941_Valid_Mountain_Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+        if n<3:
+            return False
+        i = 0 
+        while i<n-1:
+            if arr[i]>=arr[i+1]:
+                break
+            i+=1
+        if i == 0 or i == n-1:
+            return False
+        while i<n-1:
+            if arr[i]<=arr[i+1]:
+                return False
+            i+=1
+        return True
