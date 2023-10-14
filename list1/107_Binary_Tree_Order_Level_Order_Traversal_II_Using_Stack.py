21204528aee517609673242457986b89c0d9d5e0
Deepika (Sesha) Vadlamudi <deepika.vadlamudi77@gmail.com>
2023-10-13 20:20:04 -0700

    Create 107_Binary_Tree_Order_Level_Order_Traversal_II_Using_Stack.py

---
+++ list1/107_Binary_Tree_Order_Level_Order_Traversal_II_Using_Stack.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+        stack = [(root,0)]
+        while stack:
+            node, lev = stack.pop()
+            if node:
+                if len(levels)==lev:
+                    levels.append([])
+                levels[lev].append(node.val)
+                stack.append((node.right, lev+1))
+                stack.append((node.left, lev+1))
+        return levels[::-1]
+        
