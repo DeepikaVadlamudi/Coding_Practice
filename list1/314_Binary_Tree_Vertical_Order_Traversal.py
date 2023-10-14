d3cea0da26a5142222d3ae619b96e11705afe0fd
Deepika (Sesha) Vadlamudi <deepika.vadlamudi77@gmail.com>
2023-10-13 17:07:47 -0700

    Create 314_Binary_Tree_Vertical_Order_Traversal.py

---
+++ list1/314_Binary_Tree_Vertical_Order_Traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#[[3],[9,8],[4,0,1,7]]-> [[4],[9],[3,0,1],[8],[7]]
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        traversal = defaultdict(list)
+        queue = deque([(root,0)])
+        minc = maxc = 0
+        while queue:
+            node, col = queue.popleft()
+            if node:
+                queue.append((node.left, col-1))
+                queue.append((node.right, col+1))
+                traversal[col].append(node.val)
+                minc = min(minc, col)
+                maxc = max(maxc,col)
+        return [traversal[x] for x in range(minc,maxc+1)]
+        
